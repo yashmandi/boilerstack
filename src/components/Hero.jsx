086d6de83d4f786660d8110e0dbfe9d13977e74d
yashmandi <yashmandi18@gmail.com>
2024-08-04 20:01:05 +0530

    hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,11 +10,11 @@ const Hero = () => {
             className="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
             role="alert"
           >
-            <span className="text-xs bg-gray-400 rounded-full text-black px-4 py-1.5 mr-3">
+            <span className="text-xs bg-purple-400 rounded-full text-white px-4 py-1.5 mr-3">
               New
             </span>{" "}
             <span className="text-sm font-medium text-black">
-              Flowbite is out! See what's new
+              BoilerStack is out! See what's new
             </span>
             <svg
               className="ml-2 w-5 h-5"
@@ -29,19 +29,19 @@ const Hero = () => {
               ></path>
             </svg>
           </a>
-          <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none  text-white md:text-5xl lg:text-6xl ">
-            Start building in seconds, not hours.{" "}
+          <h1 className="mb-4 cursor-default text-4xl font-extrabold tracking-tight leading-none  text-white md:text-5xl lg:text-6xl ">
+            Start building in <span className="hover:text-purple-400 transition-all cursor-default">seconds</span>, not hours.{" "}
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-            Here at Flowbite we focus on markets where technology, innovation,
-            and capital can unlock long-term value and drive economic growth.
+            Fuel your app development with instant, customizable templates.
+            Choose, customize, and launch your projects swiftly and efficiently
           </p>
           <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
             <a
               href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-[#1f1f1f] bg-[#151515]  dark:focus:ring-primary-900"
             >
-              Learn more
+              Get Started
               <svg
                 className="ml-2 -mr-1 w-5 h-5"
                 fill="currentColor"
@@ -54,20 +54,6 @@ const Hero = () => {
                   clip-rule="evenodd"
                 ></path>
               </svg>
-            </a>
-            <a
-              href="#"
-              className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
-            >
-              <svg
-                className="mr-2 -ml-1 w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z"></path>
-              </svg>
-              Watch video
             </a>
           </div>
           <div className="px-4 mx-auto text-center md:max-w-screen-md lg:max-w-screen-lg lg:px-36">
