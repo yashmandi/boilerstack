788dae165b3a269ee4d543183425c52a929ad8c7
yashmandi <yashmandi18@gmail.com>
2024-08-04 19:05:56 +0530

    navbar ui

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -3,8 +3,20 @@ import React from "react";
 const NavbarComponent = () => {
   return (
     <div>
-      <div className="navbar p-4">
-        <h className="text-lg hover:bg-gray-900 cursor-default p-2 rounded-lg font-bold text-white">BoilerStack</h>
+      <div className="flex flex-row p-2">
+        <div className="navbar p-4">
+          <h className="text-md hover:bg-gray-900 cursor-default p-2 rounded-lg font-bold text-white">
+            BoilerStack
+          </h>
+        </div>
+        <div className="flex p-4">
+          <p className="text-sm hover:text-gray-100 mr-4 cursor-pointer font-semibold p-1  hover:underline  text-gray-400 transition-all">
+            Pricing
+          </p>
+          <p className="text-sm hover:text-gray-100 cursor-pointer font-semibold p-1 hover:underline text-gray-400 transition-all">
+            Get Started
+          </p>
+        </div>
       </div>
     </div>
   );
