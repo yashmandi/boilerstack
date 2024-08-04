b2bdaaf98831735bedc1cb870ee6b67243568247
yashmandi <yashmandi18@gmail.com>
2024-08-04 19:23:49 +0530

    navbar ui

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -4,23 +4,25 @@ import SearchBar from "./SearchBar";
 const NavbarComponent = () => {
   return (
     <div>
-      <div className="flex flex-row p-2">
+      <div className="flex flex-row items-center p-2 ">
         <div className="navbar p-4">
-          <h className="text-md hover:bg-gray-900 cursor-default p-2 rounded-lg font-bold text-white">
+          <h1 className="text-md hover:bg-gray-900 cursor-default p-2 rounded-lg font-bold text-white">
             BoilerStack
-          </h>
+          </h1>
         </div>
-        <div className="flex flex-between">
-          <div className="flex p-4">
-            <p className="text-sm hover:text-gray-100 mr-4 cursor-pointer font-semibold p-1  hover:underline  text-gray-400 transition-all">
+        <div className="flex-grow flex justify-between items-center p-4">
+          <div className="flex">
+            <p className="text-sm hover:text-gray-100 mr-4 cursor-pointer font-semibold p-1 hover:underline text-gray-400 transition-all">
               Pricing
             </p>
             <p className="text-sm hover:text-gray-100 cursor-pointer font-semibold p-1 hover:underline text-gray-400 transition-all">
               Get Started
             </p>
           </div>
+          <div className="flex items-center">
+            <SearchBar />
+          </div>
         </div>
-        <SearchBar />
       </div>
     </div>
   );
