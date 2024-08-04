5624911c13b2bfa2ecf459d404629ef155b1c59a
yashmandi <yashmandi18@gmail.com>
2024-08-04 11:28:38 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <p className="text-3xl font-bold underline">hello</p>
-      </div>
+      <Navbar />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
