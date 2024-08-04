293e1dc50d15f26e4883200a8fe431d8e2813222
yashmandi <yashmandi18@gmail.com>
2024-08-04 21:28:27 +0530

    landing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import BS_Logo from "../assets/BS_Logo.png";
 
-
 const Footer = () => {
   return (
     <div>
-      <footer className="bg-[#0d0d0d] text-white p-8 border-t border-gray-900">
+      <footer className="bg-[#0d0d0d] text-white p-14 border-t border-gray-900">
         <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <h2 className="flex text-2xl font-bold">
-              <span className="mr-1">
+              <span className="mr-1 ">
                 <img src={BS_Logo} className="h-8 w-8" alt="" />
-                </span>BoilerStack
-            </h2> 
+              </span>
+              BoilerStack
+            </h2>
             <p className="text-sm mt-2">
               A product by{" "}
               <a href="" className="text-blue-800 text-sm hover:underline">
@@ -30,31 +30,49 @@ const Footer = () => {
             </p>
           </div>
           <div className="flex flex-col md:flex-row text-center md:text-left">
-            <div className="mr-8">
-              <a href="/pricing" className="block mb-2">
+            <div className="mr-8 text-sm font-semibold ">
+              <a
+                href="/pricing"
+                className="block mb-2 hover:text-gray-100 text-gray-500"
+              >
                 Pricing
               </a>
-              <a href="/components" className="block mb-2">
+              <a
+                href="/components"
+                className="block mb-2 hover:text-gray-100 text-gray-500"
+              >
                 Components
               </a>
-              <a href="/templates" className="block mb-2">
+              <a
+                href="/templates"
+                className="block mb-2 hover:text-gray-100 text-gray-500"
+              >
                 Templates
               </a>
-              <a href="/categories" className="block mb-2">
+              <a
+                href="/categories"
+                className="block mb-2 hover:text-gray-100 text-gray-500"
+              >
                 Categories
               </a>
-              <a href="/blog" className="block mb-2">
+              <a
+                href="/blog"
+                className="block mb-2 hover:text-gray-100 text-gray-500"
+              >
                 Blog
               </a>
-              <a href="/box-shadows" className="block">
-                Box Shadows
-              </a>
             </div>
-            <div>
-              <a href="https://twitter.com" className="block mb-2">
+            <div className="text-sm font-semibold">
+              <a
+                href="https://twitter.com"
+                className="block mb-2 hover:text-gray-100 text-gray-500"
+              >
                 Twitter
               </a>
-              <a href="https://discord.com" className="block">
+              <a
+                href="https://discord.com"
+                className="block hover:text-gray-100 text-gray-500"
+              >
                 Discord
               </a>
             </div>
