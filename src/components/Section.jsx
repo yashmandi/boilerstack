e4f5391cba5d9537d35e6b9e9a61c3c42f7a12c7
yashmandi <yashmandi18@gmail.com>
2024-08-08 19:42:05 +0530

    hero ui

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -41,8 +41,8 @@ const Section = () => {
                 d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            <h2 className="mt-4 text-xl text-white font-medium sm:text-2xl">
-              Hover me!
+            <h2 className="mt-4 text-xl text-white font-bold sm:text-2xl">
+              Why BoilerStack?{" "}
             </h2>
           </div>
         </div>
