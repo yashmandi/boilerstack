1d163f13716800e219a475f649c3f7f0552734bc
yashmandi <yashmandi18@gmail.com>
2024-08-08 19:38:18 +0530

    link updated

--- src/components/SidebarPages.jsx/Vue3WithVueCli.jsx
+++ src/components/SidebarPages.jsx/Vue3WithVueCli.jsx
@@ -42,7 +42,7 @@ const Vue3WithVueCli = () => {
         </li>
       </ol>
       <a
-        href="https://github.com/your-repo-link"
+        href="https://github.com/yashmandi/vue3-with-cli"
         target="_blank"
         rel="noopener noreferrer"
         className="inline-flex justify-center text-sm mb-6 items-center py-2.5 px-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-[#1b1b1b] bg-[#252525] dark:focus:ring-primary-900"
