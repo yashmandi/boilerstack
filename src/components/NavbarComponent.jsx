b06b72157818e3e16657b2750216803d9203ea38
yashmandi <yashmandi18@gmail.com>
2024-08-10 17:36:33 +0530

    navbar ui fix

--- src/components/NavbarComponent.jsx
+++ src/components/NavbarComponent.jsx
@@ -13,7 +13,7 @@ const NavbarComponent = () => {
 
   return (
     <div className={`sticky top-0 z-50 backdrop-blur-lg transition-transform duration-300 ease-in-out ${menuOpen ? 'translate-y-0' : 'translate-y-0'}`}>
-      <div className="flex flex-row items-center justify-between p-4">
+      <div className="flex flex-row items-center justify-between p-2 md:p-4">
         <div className="flex items-center pl-4 md:pl-20">
           <Link to="/" className="flex items-center text-lg font-bold text-white">
             <img src={BS_Logo} className="h-8 w-8 mr-2" alt="BoilerStack Logo" />
@@ -57,9 +57,9 @@ const NavbarComponent = () => {
 
       {/* Mobile Menu */}
       <div
-        className={`md:hidden bg-[#171717] rounded-md text-white w-full flex flex-col items-start p-4 space-y-4 transition-all duration-300 ease-in-out ${menuOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}
+        className={`md:hidden bg-[#0f0f0f] rounded-md text-white w-full flex flex-col items-start p-4 space-y-4 transition-all duration-300 ease-in-out ${menuOpen ? 'max-h-screen opacity-100' : 'max-h-0 opacity-0 overflow-hidden'}`}
       >
-        <Link to="/docs" onClick={toggleMenu}>
+        <Link to="/docs"  onClick={toggleMenu}>
           Docs
         </Link>
         <Link to="/pricing" onClick={toggleMenu}>
