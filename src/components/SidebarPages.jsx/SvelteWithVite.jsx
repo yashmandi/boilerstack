4f74b8d59a023d0fe9852fffc30b8527159b78ca
yashmandi <yashmandi18@gmail.com>
2024-08-11 23:50:10 +0530

    svelte with vite

--- src/components/SidebarPages.jsx/SvelteWithVite.jsx
+++ src/components/SidebarPages.jsx/SvelteWithVite.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const SvelteWithCli = () => {
+const SvelteWithVite = () => {
   return (
     <div>
       
@@ -8,4 +8,4 @@ const SvelteWithCli = () => {
   )
 }
 
-export default SvelteWithCli
+export default SvelteWithVite
