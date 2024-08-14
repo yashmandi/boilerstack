4b848e4a1e9822534f5aace92ec0333c189d3917
yashmandi <yashmandi18@gmail.com>
2024-08-14 13:23:47 +0530

    search bar func

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -45,11 +45,11 @@ const SearchBar = ({ placeholder }) => {
         />
       </form>
       {filteredOptions.length > 0 && (
-        <div className="absolute left-0 right-0 mt-2 bg-[#1d1d1d] border border-gray-600 rounded-lg shadow-lg z-10">
+        <div className="absolute left-0 w-[220px] right-0 mt-2 bg-[#1d1d1d] border border-gray-900 transition-all rounded-lg shadow-lg z-10">
           {filteredOptions.map((option, index) => (
             <div
               key={index}
-              className="px-4 py-2 cursor-pointer hover:bg-gray-700 text-white"
+              className="px-3 py-1.5 text-sm cursor-pointer hover:bg-[#171717] hover:rounded-lg text-white"
               onClick={() => handleOptionClick(option.route)}
             >
               {option.name}
